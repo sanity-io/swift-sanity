81566dcabf89592776e50e6a04fce4ae2d3ceff8
Rune Botten <rbotten@gmail.com>
2021-05-06 18:00:18 -0700

    Create .swiftformat to have consistent formatting from IDEs too

--- format.sh
+++ format.sh
@@ -3,21 +3,4 @@ if [ "$#" -eq 0 ]; then
     files=(Sources/ Tests/ Example/)
 fi
 
-YEAR=`date "+%Y"`
-if [ $YEAR = "2021" ]; then
-    YEARSTR="2021"
-else
-    YEARSTR="2021 - $YEAR"
-fi
-
-copyright="
-// MIT License
-// 
-// Copyright (c) $YEARSTR Sanity.io
-"
-
-swiftformat \
-    --swiftversion 5.4 \
-    --disable redundantSelf,enumNamespaces,redundantFileprivate \
-    --header "$copyright" \
-    "${files[@]}"
+swiftformat "${files[@]}"
