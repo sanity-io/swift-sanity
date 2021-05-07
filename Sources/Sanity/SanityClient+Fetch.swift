183745f504b65c3b0d0eed190603bf99632cf368
Rune Botten <rbotten@gmail.com>
2021-05-07 12:38:54 -0700

    Format

--- Sources/Sanity/SanityClient+Fetch.swift
+++ Sources/Sanity/SanityClient+Fetch.swift
@@ -25,40 +25,35 @@ public extension SanityClient.Query {
 
     struct ErrorResponse: Decodable {
         enum keys: String, CodingKey { case message, statusCode, error }
-        
+
         let message: String
         let statusCode: Int
         let errorMessage: String
-        
+
         public init(from decoder: Decoder) throws {
             let container = try decoder.container(keyedBy: keys.self)
             self.message = try container.decode(String.self, forKey: .message)
             self.statusCode = try container.decode(Int.self, forKey: .statusCode)
             self.errorMessage = try container.decode(String.self, forKey: .error)
         }
-        
+
         struct Error: LocalizedError {
             var errorResponse: ErrorResponse
             init(response: ErrorResponse) {
                 self.errorResponse = response
             }
+
             var recoverySuggestion: String? {
-                get {
-                    return self.errorResponse.message
-                }
+                self.errorResponse.message
             }
-            
+
             var failureReason: String? {
-                get {
-                    return self.errorResponse.errorMessage
-                }
-            }            
-        }
-        
-        var error: Error {
-            get {
-                return Error(response: self)
+                self.errorResponse.errorMessage
             }
+        }
+
+        var error: Error {
+            Error(response: self)
         }
     }
 
@@ -71,9 +66,9 @@ public extension SanityClient.Query {
             }
 
             switch httpResponse.statusCode {
-            case 200..<300:
+            case 200 ..< 300:
                 return data
-            case 400..<500:
+            case 400 ..< 500:
                 let errorEnvelope = try JSONDecoder().decode(ErrorResponse.self, from: data)
                 throw errorEnvelope.error
             default:
@@ -87,29 +82,28 @@ public extension SanityClient.Query {
     func fetch(completion: @escaping ResultCallback<DataResponse<T>>) {
         let urlRequest = apiURL.fetch(query: query, params: params, config: config).urlRequest
 
-        let task = urlSession.dataTask(with: urlRequest) { data, response, error in
+        let task = urlSession.dataTask(with: urlRequest) { data, response, _ in
             guard let httpResponse = response as? HTTPURLResponse, let data = data else {
                 return completion(.failure(URLError(.badServerResponse)))
             }
-            
+
             let decoder = JSONDecoder()
-            
+
             switch httpResponse.statusCode {
-            case 200..<300:
+            case 200 ..< 300:
                 do {
                     let data = try decoder.decode(DataResponse<T>.self, from: data)
                     completion(.success(data))
                 } catch let e {
                     completion(.failure(e))
                 }
-            case 400..<500:
+            case 400 ..< 500:
                 if let errorEnvelope = try? JSONDecoder().decode(ErrorResponse.self, from: data) {
                     return completion(.failure(errorEnvelope.error))
                 }
                 fallthrough
             default:
                 return completion(.failure(URLError(.badServerResponse)))
-                
             }
         }
 
