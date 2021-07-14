4287c4e350a76b2522cb48b138858a1a0c0aa607
Sindre Gulseth <sgulseth@gmail.com>
2021-07-14 22:05:57 +0200

    feature(types): add weak type to ref

--- Sources/Sanity/SanityType+Ref.swift
+++ Sources/Sanity/SanityType+Ref.swift
@@ -6,15 +6,18 @@ public extension SanityType {
     struct Ref {
         public let _ref: String
         public let _type: String
+        public let _weak: Bool?
 
-        init(_ref: String, _type: String) {
+        public init(_ref: String, _type: String, _weak: Bool? = nil) {
             self._ref = _ref
             self._type = _type
+            self._weak = _weak
         }
     }
 }
 
 extension SanityType.Ref: Codable {}
+
 extension SanityType.Ref: Hashable {}
 extension SanityType.Ref: Equatable {
     public static func == (lhs: Self, rhs: Self) -> Bool {
