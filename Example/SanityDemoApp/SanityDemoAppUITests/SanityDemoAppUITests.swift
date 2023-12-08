6dcf1eb9faf94ec872401fd3201f51a90841a6ad
Sindre Gulseth <sgulseth@gmail.com>
2023-12-08 22:17:03 +0100

    test: add test for data render

--- Example/SanityDemoApp/SanityDemoAppUITests/SanityDemoAppUITests.swift
+++ Example/SanityDemoApp/SanityDemoAppUITests/SanityDemoAppUITests.swift
@@ -30,6 +30,20 @@ class SanityDemoAppUITests: XCTestCase {
         XCTAssert(app.staticTexts["Movies"].waitForExistence(timeout: 10))
     }
 
+    func testRendersData() throws {
+        // UI tests must launch the application that they test.
+        let app = XCUIApplication()
+        app.launch()
+
+        // Use recording to get started writing UI tests.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+
+        let predicate = NSPredicate(format: "label CONTAINS[c] %@", "is the last robot left on an earth")
+        let element = app.staticTexts.element(matching: predicate)
+
+        XCTAssert(element.waitForExistence(timeout: 10))
+    }
+
     func testLaunchPerformance() throws {
         if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
             // This measures how long it takes to launch your application.
