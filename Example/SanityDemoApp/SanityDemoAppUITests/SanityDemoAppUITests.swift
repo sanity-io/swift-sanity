ad878733051984eccfce1e2ac4d0815577fb0fe5
Sindre Gulseth <sgulseth@gmail.com>
2023-12-08 22:17:33 +0100

    test: remove performance test, unnecessary & slow

--- Example/SanityDemoApp/SanityDemoAppUITests/SanityDemoAppUITests.swift
+++ Example/SanityDemoApp/SanityDemoAppUITests/SanityDemoAppUITests.swift
@@ -43,13 +43,4 @@ class SanityDemoAppUITests: XCTestCase {
 
         XCTAssert(element.waitForExistence(timeout: 10))
     }
-
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, watchOS 7.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
 }
